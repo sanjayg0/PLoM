1904fc8908ffd001bd6fbe2ff484fa5ed966827c
Javi Gual <javi.gual@gmail.com>
2021-10-20 22:40:58 +0200

    scaling beta

--- example/example0_constraints/data/constraints_example2D.py
+++ example/example0_constraints/data/constraints_example2D.py
@@ -12,6 +12,12 @@ def beta_c():
     beta_c = np.array([[3],[5]])
     return beta_c
 
+def beta_c_aux(beta_c, x_min, alpha):
+    beta_c_aux = np.zeros(beta_c.shape)
+    for i in range(0, beta_c.shape[0]):
+        beta_c_aux[i] = (beta_c[i] -x_min[i])/alpha[i]        
+    return beta_c_aux
+
 def D_x_g_c(x): #x can be a column vector or a matrix
     D = np.zeros((x.shape[0],2))
     D[0,0] = 1
